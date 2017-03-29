0e82e176ca7f23d003d9c68451c1f2a7206b3f31
王戎 <88343264@qq.com>
2017-03-29 16:43:46 +0800

    添加MVP

---
+++ app/src/main/java/com/changxun/anew/myqrcode/feature/MainPresenter.java
@@ -0,0 +1,46 @@
+package com.changxun.anew.myqrcode.feature;
+
+import com.changxun.anew.myqrcode.model.Contract;
+import com.changxun.anew.myqrcode.network.HttpUtil;
+import com.changxun.anew.myqrcode.network.service.ContractService;
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainPresenter {
+
+  private Throwable mThrowable;
+  private MainActivity mView;
+  private Contract mContract;
+  private Request mRequest;
+
+  public MainPresenter(MainActivity view) {
+    Request.Builder builder = new Request.Builder();
+    builder.url(ContractService.daily_url);
+    mRequest = builder.build();
+    mView = view;
+  }
+
+  public void request() {
+    HttpUtil.enqueue(mRequest, new Callback() {
+      @Override public void onFailure(Call call, IOException e) {
+        mView.onError(e);
+      }
+
+      @Override public void onResponse(Call call, Response response) throws IOException {
+        mView.onSuccess(mContract);
+      }
+    });
+  }
+
+  public void onDestroy() {
+    mThrowable = null;
+    mView = null;
+    mContract = null;
+    mRequest = null;
+  }
+
+}
